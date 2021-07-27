39e7a7c208bc2c7972108d2d52242ac9cd254a66
Chris Tavares <cct@tavaresstudios.com>
2021-07-26 22:05:11 -0700

    Test for required flag matching

--- tests/requiredflag-tests.js
+++ tests/requiredflag-tests.js
@@ -12,3 +12,13 @@ test('required bool flag has expected error if not present', t => {
 
   t.is(reqFlag.error, "Required flag '-t|--test' not given")
 })
+
+test('required bool flag does not error if present', t => {
+  const bflag = boolFlag('-t', null)
+  const reqFlag = requiredFlag(bflag)
+
+  parseArgs([reqFlag], ['-t'])
+
+  t.falsy(reqFlag.error)
+  t.true(reqFlag.matched)
+})
